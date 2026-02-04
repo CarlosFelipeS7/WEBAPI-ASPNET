9da69dd09348a92969ce46281bb01e89a5ac5767
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-02-04 16:28:05 -0300

    Aplicando Data Annotations

--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,9 +1,21 @@
-﻿namespace APICatalogo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
+
+    [Table("Categorias")] //Anotação para definir o nome da tabela no banco de dados
     public class Categoria
     {
+        [Key]//Anotação para definir a chave primaria, mas é opcional nesse caso
         public int CategoriaId { get; set; } //Entity Framework entende que Id= chave primaria, e nome da classe+id tambem (Categoria+Id)
+
+        [Required]
+        [StringLength(50)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
         public ICollection<Produto>? Produtos { get; set; } //propriedade de navegação para representar o relacionamento entre Categoria e Produto
--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,10 +1,24 @@
-﻿namespace APICatalogo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
+    [Table("Produtos")] //Anotação para definir o nome da tabela no banco de dados
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? Descricao { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Preco { get; set; }
         public string? ImagemUrl { get; set; }
         public int Estoque { get; set; }
