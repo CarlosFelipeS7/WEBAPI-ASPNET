3c8a829d9cc4f967744aa3cbb9306c9f0f8b1754
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-02-05 17:06:28 -0300

    Action Methods Delete

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -57,6 +57,20 @@ namespace APICatalogo.Controllers
             _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             _context.SaveChanges();
             return Ok(produto);
-        } 
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            var produto = _context.Produtos.Find(id);
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado para o id informado");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+
     }
 }
